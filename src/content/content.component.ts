a70728531712953e98545f5c1017149e83bbad23
Weberth Erik Anolar Sirqueira <51481418+Weberth1233@users.noreply.github.com>
2021-08-17 16:06:34 -0300

    Adc filtro e categoria na noticia Adicionado o filtro pela categoria e inserindo o novo atributo categoria a noticia

--- src/content/content.component.ts
+++ src/content/content.component.ts
@@ -10,6 +10,7 @@ export class ContentComponent {
     {
       Titulo: "Guerra no Pacífico",
       DataPublic: "26/09/2000",
+      Categoria: "Mundo",
       Desc:
         "A guerra começou e já deixou mais de 10.000 mil feridos ao redor de todo o país.",
       Criador: "Weberth Erik"
@@ -17,6 +18,7 @@ export class ContentComponent {
     {
       Titulo: "Inglaterra perde em casa",
       DataPublic: "26/09/2000",
+      Categoria: "Esporte",
       Desc:
         "O jogo aconteceu nesse domingo e o campeão foi decidido nos penaltins levando a taça a grande Italia",
       Criador: "Erik Junior"
@@ -24,30 +26,39 @@ export class ContentComponent {
     {
       Titulo: "Disputa pelo 5G",
       DataPublic: "26/09/2000",
+      Categoria: "Tecnologia",
       Desc:
         "A guerra começou e já deixou mais de 10.000 mil feridos ao redor de todo o país",
       Criador: "Bruno Aguiar"
     },
     {
-      Titulo: "Nova notícia adicionada",
+      Titulo: "Novo filme do Esquadrão Galático",
       DataPublic: "26/09/2000",
-      Desc:
-        "A guerra começou e já deixou mais de 10.000 mil feridos ao redor de todo o país",
+      Categoria: "Entreterimento",
+      Desc: "Novo filme do Esquadrão Galático está nos cimenas já!",
       Criador: "Carlos Dias"
     },
     {
-      Titulo: "Noticia teste adc",
+      Titulo: "Dolar em alta",
       DataPublic: "22/04/2020",
-      Desc:
-        "A guerra começou e já deixou mais de 10.000 mil feridos ao redor de todo o país",
+      Categoria: "Brasil",
+      Desc: "O valor da moeda americana está valendo 5,50",
       Criador: "Teste"
     },
     {
       Titulo: "Flamendo golea novamente",
       DataPublic: "13/08/2021",
+      Categoria: "Esporte",
       Desc:
         "Flamendo vence Olimpia por 4 a 1 em pleno estádio dos adversários.",
       Criador: "Weberth Erik"
     }
   ];
+  Filtrar(categoria: string) {
+    this.noticias.forEach((element) => {
+      if (element.Categoria === categoria) {
+        console.log(element.Titulo);
+      }
+    });
+  }
 }
